78381266f11a7d0f37c977f7b64a39ecd8fcb202
jonathan-crepeau <jon.crepeau@gmail.com>
2023-01-07 21:26:29 -0800

    if/else + switch alternative complete

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,40 @@
 console.log('app.js reporting for duty..');
+const typeOfNum = (inputNum) => {
+    if (inputNum < 10) {
+        console.log(inputNum, 'is a small number.');
+    }
+    else if (inputNum < 50) {
+        console.log(inputNum, 'is a medium-sized number.');
+    }
+    else if (inputNum < 100) {
+        console.log(inputNum, 'is a large number.');
+    }
+    else if (inputNum > 100) {
+        celebrateBigNum(inputNum);
+    }
+};
+const celebrateBigNum = (inputNum) => console.log(inputNum, 'IS A GIANT NUMBER!');
+// typeOfNum(7);
+// typeOfNum(12);
+// typeOfNum(77);
+// typeOfNum(101);
+const checkANum = (inputNum) => {
+    switch (true) {
+        case (inputNum < 10):
+            console.log(inputNum, 'is a small number!');
+            break;
+        case (inputNum < 50):
+            console.log(inputNum, 'is a medium-sized number!');
+            break;
+        case (inputNum < 100):
+            console.log(inputNum, 'is a large number!');
+            break;
+        case (inputNum > 100):
+            celebrateBigNum(inputNum);
+            break;
+    }
+};
+checkANum(7);
+checkANum(12);
+checkANum(75);
+checkANum(300);
