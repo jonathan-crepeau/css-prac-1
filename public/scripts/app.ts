dc9ccca6e5ce86f80b0c7b803e411813d7a21ad0
jonathan-crepeau <jon.crepeau@gmail.com>
2023-01-08 23:36:18 -0800

    fizzbuzz exercise complete

--- public/scripts/app.ts
+++ public/scripts/app.ts
@@ -46,8 +46,36 @@ const checkANum = (inputNum) => {
 
 // SECTION - Looping A Triangle
 
-let toPrintVal = '#';
-for (let i = 0; i < 7; i++) {
-    console.log(toPrintVal);
-    toPrintVal += '#';
+// let toPrintVal = '#';
+// for (let i = 0; i < 7; i++) {
+//     console.log(toPrintVal);
+//     toPrintVal += '#';
+// }
+
+
+// SECTION - FizzBuzz
+
+const fizzbuzz = () => {
+    for (let i = 1; i < 101; i++) {
+        if (i % 15 === 0) {
+            console.log('FizzBuzz');
+        } else if (i % 3 === 0)  {
+            console.log('fizz');
+        } else if (i % 5 === 0) {
+            console.log('buzz');
+        } else {
+            console.log(i);
+        }
+    }
+};
+
+
+const shortModern = (n) => {
+    for (let i = 0; i < n; ) {
+        console.log(
+            (++i % 3 ? "" : "fizz") + (i % 5 ? "" : "buzz") || i            
+        );
+    }
 }
+
+shortModern(101);
