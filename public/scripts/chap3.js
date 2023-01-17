e5c795bf8501eb8899b871d31df84e4e074436c9
jonathan-crepeau <jon.crepeau@gmail.com>
2023-01-17 15:26:52 -0800

    TDZ exercises copmlete

--- public/scripts/chap3.js
+++ public/scripts/chap3.js
@@ -16,4 +16,41 @@ function lunaNoises(x) {
     }
     return string;
 }
-console.log(lunaNoises(5));
+// console.log(lunaNoises(5));
+// ANCHOR Temporal Dead Zone Exercises:
+// SECTION - Let Keyword
+// NOTE - Geting 'undefined' in TDZ:
+// {
+//     let myPet;
+//     console.log(myPet);
+//         // returns 'undefined' for being declared with let (above) but not completely initialized --> defaults to 'undefined'
+//     myPet = 'Luna';
+//     console.log(myPet);
+//         // returns 'Luna' for the variable having been completely intialized.
+// }
+// NOTE - Getting ReferenceError in TDZ:
+// {
+//     console.log(myDog);
+//     let myDog = 'tess';
+// }
+// SECTION - Const Keyword: 
+// NOTE - Cannot declare with 'const' keyword and not completely initialize the variable:
+// {
+//     const myMom;
+//         // throws Uncaught Syntax error for not being initalized at declaration.
+//     console.log(myMom);
+// }
+// {
+//     console.log(myDad);
+//         // throws ReferenceError for trying to access before initialization
+//     const myDad = 'Bruce';
+// }
+// SECTION - Var Keyword
+// {
+//     console.log(myFriend);
+//         // returns undefined because of 'var' hoisting, not throwing a ReferenceError like 'let' and 'const' if you try to access a variable before it is declared.
+//     var myFriend;
+//     myFriend = 'Annie';
+//     console.log(myFriend);
+//         // returns 'Annie'
+// }
