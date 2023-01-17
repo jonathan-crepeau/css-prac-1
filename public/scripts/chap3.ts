38bcf026d42cf228899d586952d807fa6f12d698
jonathan-crepeau <jon.crepeau@gmail.com>
2023-01-17 12:33:47 -0800

    function definition and declaration exercises complete

---
+++ public/scripts/chap3.ts
@@ -0,0 +1,26 @@
+// console.log('chap3.js reporting for duty..');
+
+// NOTE - Funciton Definition (below). Cannot be called in a program unless the call is after the definition.
+
+let lunaMeows = function(n: number) {
+    let string = '';
+    for (let i = 0; i < n; i++) {
+        string += "meow ";
+    }
+    return string;
+}
+
+// console.log(lunaMeows(11));
+
+
+// NOTE - Function Declaration (below). Function declarations are not part of the regular top-to-bottom flow of control. They are hoisted at runtime(?) and as a result can be called anywhere in the program.
+
+function lunaNoises(x) {
+    let string = '';
+    for (let i = 0; i < x; i++) {
+        string += 'Meow ';
+    }
+    return string;
+}
+
+console.log(lunaNoises(5));
